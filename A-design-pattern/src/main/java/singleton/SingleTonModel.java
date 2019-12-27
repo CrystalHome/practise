72d37ea280f39c8a018943aeb9d4ed3c7d13e9b7
陈之晶 <crystal_471006350@126.com>
2019-12-28 02:23:42 +0800

    添加单例模式包路径

---
+++ A-design-pattern/src/main/java/singleton/SingleTonModel.java
@@ -0,0 +1,11 @@
+package singleton;
+
+/**
+ * @author Crystal-Chen
+ * @Title: SingleTonModel
+ * @Package singleton
+ * @Description:
+ * @date 2019/12/28 2:22
+ */
+public class SingleTonModel {
+}
